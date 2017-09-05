1e97bfe8050b7c22c299d8a447cbbd17b19cdd13
pucumt <weple@qq.com>
2017-09-05 14:45:42 +0800

    xue qing

---
+++ GenerateEnrollExcel/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace GenerateEnrollExcel
+{
+    partial class Frm_Generator
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_generator = new System.Windows.Forms.Button();
+            this.ofD_file = new System.Windows.Forms.OpenFileDialog();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btn_select = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_generator
+            // 
+            this.btn_generator.Location = new System.Drawing.Point(67, 80);
+            this.btn_generator.Name = "btn_generator";
+            this.btn_generator.Size = new System.Drawing.Size(75, 30);
+            this.btn_generator.TabIndex = 0;
+            this.btn_generator.Text = "生 成";
+            this.btn_generator.UseVisualStyleBackColor = true;
+            this.btn_generator.Click += new System.EventHandler(this.btn_generator_Click);
+            // 
+            // ofD_file
+            // 
+            this.ofD_file.FileName = "报名情况5.xlsx";
+            this.ofD_file.Filter = "*.xls|*.xlsx";
+            // 
+            // txtFile
+            // 
+            this.txtFile.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtFile.Location = new System.Drawing.Point(67, 32);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.ReadOnly = true;
+            this.txtFile.Size = new System.Drawing.Size(312, 27);
+            this.txtFile.TabIndex = 1;
+            // 
+            // btn_select
+            // 
+            this.btn_select.Location = new System.Drawing.Point(385, 31);
+            this.btn_select.Name = "btn_select";
+            this.btn_select.Size = new System.Drawing.Size(75, 30);
+            this.btn_select.TabIndex = 2;
+            this.btn_select.Text = "选择...";
+            this.btn_select.UseVisualStyleBackColor = true;
+            this.btn_select.Click += new System.EventHandler(this.btn_select_Click);
+            // 
+            // Frm_Generator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 138);
+            this.Controls.Add(this.btn_select);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.btn_generator);
+            this.MaximizeBox = false;
+            this.Name = "Frm_Generator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "生成学情表";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_generator;
+        private System.Windows.Forms.OpenFileDialog ofD_file;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btn_select;
+    }
+}
+
---
+++ GenerateEnrollExcel/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using NPOI.XSSF.UserModel;
+using System.Data.OleDb;
+using System.Threading;
+
+namespace GenerateEnrollExcel
+{
+    public partial class Frm_Generator : Form
+    {
+        public Frm_Generator()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_select_Click(object sender, EventArgs e)
+        {
+            //var result = ofD_file.ShowDialog();
+            //txtFile.Text = ofD_file.FileName;
+        }
+
+        private void btn_generator_Click(object sender, EventArgs e)
+        {
+            var dbHelper = new DBHelper();
+            var courses = dbHelper.getAllCourses();
+
+            foreach (var curClass in courses)
+            {
+                BsonValue teacherId;
+                if (!curClass.TryGetValue("teacherId", out teacherId))
+                {
+                    continue;
+                }
+
+                XSSFWorkbook hssfworkbook = null;
+                using (FileStream fs = File.Open(@"template.xlsx", FileMode.Open,
+                FileAccess.Read, FileShare.ReadWrite))
+                {
+                    //把xls文件读入workbook变量里，之后就可以关闭了  
+                    hssfworkbook = new XSSFWorkbook(fs);
+                    fs.Close();
+                }
+
+                var fileName = string.Format("沟通_{0}_{1}_{2}_{3}.xlsx", curClass.GetValue("schoolArea"), curClass.GetValue("subjectName"), curClass.GetValue("gradeName"), curClass.GetValue("name"));
+
+                XSSFSheet sheet1 = hssfworkbook.GetSheet("template") as XSSFSheet;
+                hssfworkbook.SetSheetName(0, curClass.GetValue("name").ToString());
+
+                var curSource = dbHelper.getAllSources(curClass.GetValue("_id").ToString());
+                var curTeacher = dbHelper.getTeacher(teacherId.ToString());
+                
+                for (var i = 0; i < curSource.Count; i++)
+                {
+                    var curStudent = dbHelper.getStudent(curSource[i].GetValue("studentId").ToString());
+
+                    sheet1.GetRow(i + 2 + 6 * i).GetCell(1).SetCellValue(curClass.GetValue("name").ToString());
+                    sheet1.GetRow(i + 2 + 6 * i).GetCell(2).SetCellValue(curStudent.GetValue("name").ToString());
+                    sheet1.GetRow(i + 2 + 6 * i).GetCell(3).SetCellValue(curClass.GetValue("teacherName").ToString());
+                    sheet1.GetRow(i + 2 + 6 * i).GetCell(4).SetCellValue(curStudent.GetValue("mobile").ToString());
+                    sheet1.GetRow(i + 2 + 6 * i).GetCell(5).SetCellValue(curTeacher.GetValue("mobile").ToString());
+                }
+
+                sheet1.ForceFormulaRecalculation = true;
+
+                using (FileStream fileStream = File.Open(fileName,
+                    FileMode.Create, FileAccess.ReadWrite))
+                {
+                    hssfworkbook.Write(fileStream);
+                    fileStream.Close();
+                }
+
+            }
+            MessageBox.Show("导出成功！");
+        }
+
+        public List<Order> ExcelToDS(string Path)
+        {
+            FileStream file = new FileStream(Path, FileMode.Open, FileAccess.Read);
+            XSSFWorkbook hssfworkbook = new XSSFWorkbook(file);
+            XSSFSheet sheet1 = hssfworkbook.GetSheet("报名情况") as XSSFSheet;
+            List<Order> orders = new List<Order>();
+            var i = 1;
+            while(sheet1.GetRow(i)!=null)
+            {
+                var newOrder = new Order();
+                for (var j = 0; j < 11; j++)
+                {
+                    newOrder.studentName = sheet1.GetRow(i).GetCell(0).StringCellValue;
+                    newOrder.mobile = sheet1.GetRow(i).GetCell(1).StringCellValue;
+                    newOrder.className = sheet1.GetRow(i).GetCell(8).StringCellValue;
+                    newOrder.school = sheet1.GetRow(i).GetCell(7).StringCellValue;
+                    newOrder.grade = sheet1.GetRow(i).GetCell(10).StringCellValue;
+                    newOrder.subject = sheet1.GetRow(i).GetCell(6).StringCellValue;
+                    newOrder.courseTime = sheet1.GetRow(i).GetCell(9).StringCellValue;
+                    newOrder.studentSchool = (sheet1.GetRow(i).GetCell(2)!=null?sheet1.GetRow(i).GetCell(2).StringCellValue:"");
+                    newOrder.studentClass = (sheet1.GetRow(i).GetCell(3)!=null?sheet1.GetRow(i).GetCell(3).StringCellValue:"");
+                }
+                if(newOrder.className!="")
+                {
+                    orders.Add(newOrder);
+                }                
+                i++;
+            }
+
+             return orders;
+        }
+    }
+
+    public class Order
+    {
+        public string studentName;
+        public string mobile;
+        public string studentSchool;
+        public string studentClass;
+        public string sex;
+        public string subject;
+        public string school;
+        public string className;
+        public string grade;
+        public string courseTime;
+    }
+
+    public class Course
+    {
+        public string _Id;
+        public string name;
+        public string schoolArea;
+        public string subjectName;
+        public string gradeName;
+    }
+
+    public class DBHelper
+    {
+        //连接信息
+        string conn = "mongodb://127.0.0.1:10105";
+        string database = "websiteOnline2";
+        MongoClient mongodb;
+        IMongoDatabase mongoDataBase;
+
+
+        public DBHelper() {
+            mongodb = new MongoClient(conn);//连接数据库
+            mongoDataBase = mongodb.GetDatabase(database);//选择数据库名
+        }
+
+        public List<BsonDocument> getAllCourses()
+        {
+            IMongoCollection<BsonDocument> mongoCollection = mongoDataBase.GetCollection <BsonDocument> ("trainClasss");//选择集合，相当于表
+
+            //var count = mongoCollection.Count(new BsonDocument());
+
+            QueryDocument query = new QueryDocument();
+            BsonDocument b = new BsonDocument();
+            b.Add("$ne", true);
+            query.Add("isDeleted", b);
+            query.Add("yearId", "591421b172a6d61d0aee0f3d");
+
+            return mongoCollection.Find(query).ToList();
+        }
+
+        public List<BsonDocument> getAllSources(string classId)
+        {
+            IMongoCollection<BsonDocument> mongoCollection = mongoDataBase.GetCollection<BsonDocument>("adminEnrollTrains");//选择集合，相当于表
+
+            QueryDocument query = new QueryDocument();
+            query.Add("isSucceed", 1);
+            query.Add("trainId", new ObjectId(classId));
+
+            return mongoCollection.Find(query).ToList();
+        }
+
+        public BsonDocument getTeacher(string teacherId)
+        {
+            IMongoCollection<BsonDocument> mongoCollection = mongoDataBase.GetCollection<BsonDocument>("teachers");//选择集合，相当于表
+
+            QueryDocument query = new QueryDocument();
+            BsonDocument b = new BsonDocument();
+            b.Add("$ne", true);
+            query.Add("isDeleted", b);
+            query.Add("_id", new ObjectId(teacherId));
+
+            return mongoCollection.Find(query).FirstOrDefault();
+        }
+
+        public BsonDocument getStudent(string studentId)
+        {
+            IMongoCollection<BsonDocument> mongoCollection = mongoDataBase.GetCollection<BsonDocument>("studentInfos");//选择集合，相当于表
+
+            QueryDocument query = new QueryDocument();
+            BsonDocument b = new BsonDocument();
+            b.Add("$ne", true);
+            query.Add("isDeleted", b);
+            query.Add("_id", new ObjectId(studentId));
+
+            return mongoCollection.Find(query).FirstOrDefault();
+        }
+
+        public void Dispose() {
+            
+        }
+    }
+}
